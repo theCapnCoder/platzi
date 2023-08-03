418f11da51c3b7b6c91252b773059a3efc3ccead
PavelM <myagkiypavel@gmail.com>
2023-08-03 13:45:43 +0300

    chore: App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,8 +3,17 @@ import AppRoutes from "../Routes/Routes";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Sidebar from "../Sidebar/Sidebar";
+import { useEffect } from "react";
+import { getCategories } from "../../features/categories/categoriesSlice";
+import { useAppDispatch } from "../../features/hook";
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getCategories());
+  }, [dispatch]);
+
   return (
     <Box>
       <Header />
