7e9025af224ac9f9ae404e7221d82766f85dd869
PavelM <myagkiypavel@gmail.com>
2023-08-03 13:43:23 +0300

    chore: index

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./features/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
