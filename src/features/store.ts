618e3f7af29de2121127641ab76b7c4b8daafd7f
PavelM <myagkiypavel@gmail.com>
2023-08-04 11:38:50 +0300

    feature: apply query

--- src/features/store.ts
+++ src/features/store.ts
@@ -1,4 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./api/apiSlice";
+
 import categoriesSlice from "./categories/categoriesSlice";
 import productsSlice from "./products/productsSlice";
 
@@ -6,7 +8,9 @@ export const store = configureStore({
   reducer: {
     categories: categoriesSlice,
     products: productsSlice,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
-  devTools: true
-
-})
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
+});
