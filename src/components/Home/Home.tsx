cba3199c646037ade5ff623ce5f28167f2d4e07d
PavelM <myagkiypavel@gmail.com>
2023-08-04 11:40:02 +0300

    chore: Home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { Box, Typography } from "@mui/material";
+import Products from "../Products/Products";
+import { useAppDispatch, useAppSelector } from "../../features/hook";
+import Categories from "../Categories/Categories";
+import { useEffect } from "react";
+import { filterByPrice } from "../../features/products/productsSlice";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const {products,  products: {list, filtered}, categories} = useAppSelector((state) => state);
+  const dispatch = useAppDispatch();
 
-export default Home
+  useEffect(() => {
+    if(!list.length) return;
+
+    dispatch(filterByPrice(100))
+    
+  }, [dispatch, list.length]);
+
+  return (
+    <Box>
+      <Products products={products.list} amount={10} title="Products" />
+      <Categories products={categories.list} amount={5} />
+      <Products products={filtered} amount={5} title="Less then 100$" />
+    </Box>
+  );
+};
+
+export default Home;
