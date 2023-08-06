a1ccef45f30e4b3678b583b5a34dd6d7dd63a1e3
PavelM <myagkiypavel@gmail.com>
2023-08-06 17:21:57 +0300

    chore: Search

--- src/components/Header/Search.tsx
+++ src/components/Header/Search.tsx
@@ -1,27 +1,58 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Popper from '@mui/material/Popper';
+import {useState} from "react";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Popper from "@mui/material/Popper";
+import PopupState, { bindToggle, bindPopper } from "material-ui-popup-state";
+import Fade from "@mui/material/Fade";
+import Paper from "@mui/material/Paper";
+import { Popover, TextField } from "@mui/material";
 
-export default function Search() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+export default function PopperPopupState() {
+   const [inputValue, setInputValue] = useState('');
+  const [isPopoverOpen, setPopoverOpen] = useState(false);
 
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(anchorEl ? null : event.currentTarget);
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+    setPopoverOpen(event.target.value.trim() !== '');
   };
 
-  const open = Boolean(anchorEl);
-  const id = open ? 'simple-popper' : undefined;
+  const handlePopoverClose = () => {
+    setPopoverOpen(false);
+  };
 
   return (
     <div>
-      <button aria-describedby={id} type="button" onClick={handleClick}>
-        Toggle Popper
-      </button>
-      <Popper id={id} open={open} anchorEl={anchorEl}>
-        <Box sx={{ border: 1, p: 1, bgcolor: 'background.paper' }}>
-          The content of the Popper.
-        </Box>
-      </Popper>
+      <TextField
+        label="First Name"
+        variant="outlined"
+        fullWidth
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <Popover
+        open={isPopoverOpen}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+        onClose={handlePopoverClose}
+      >
+        <div style={{ padding: 16 }}>
+          <TextField label="Last Name" variant="outlined" fullWidth />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handlePopoverClose}
+            style={{ marginTop: 16 }}
+          >
+            Save
+          </Button>
+        </div>
+      </Popover>
     </div>
   );
 }
