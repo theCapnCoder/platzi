0de6380a870469d7613b247edae95c7584c89fb0
PavelM <myagkiypavel@gmail.com>
2023-08-07 10:37:43 +0300

    fix: Search component

--- src/components/Header/Search.tsx
+++ src/components/Header/Search.tsx
@@ -1,58 +1,94 @@
-import {useState} from "react";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import Popper from "@mui/material/Popper";
-import PopupState, { bindToggle, bindPopper } from "material-ui-popup-state";
-import Fade from "@mui/material/Fade";
-import Paper from "@mui/material/Paper";
-import { Popover, TextField } from "@mui/material";
+import React, { useState } from "react";
+import { useGetProductsQuery } from "../../features/api/apiSlice";
+import { Box, Paper, Stack, TextField, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
-export default function PopperPopupState() {
-   const [inputValue, setInputValue] = useState('');
-  const [isPopoverOpen, setPopoverOpen] = useState(false);
+type Category = {
+  id: number;
+  name: string;
+  image: string;
+  creationAt: string;
+  updatedAt: string;
+};
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value);
-    setPopoverOpen(event.target.value.trim() !== '');
-  };
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  images: string[];
+  creationAt: string;
+  updatedAt: string;
+  category: Category;
+};
 
-  const handlePopoverClose = () => {
-    setPopoverOpen(false);
+const Search = () => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const {
+    data: products = [],
+    isLoading,
+    isSuccess,
+  } = useGetProductsQuery({
+    title: searchValue,
+  });
+  console.log("products", products);
+
+  const handleValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
   };
 
   return (
-    <div>
+    <Box>
       <TextField
-        label="First Name"
-        variant="outlined"
-        fullWidth
-        value={inputValue}
-        onChange={handleInputChange}
+        placeholder="Search"
+        onChange={handleValueChange}
+        sx={{
+          position: "relative",
+          width: 300,
+          "& .MuiInputBase-input": { py: 1 },
+        }}
       />
-      <Popover
-        open={isPopoverOpen}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'center',
-        }}
-        onClose={handlePopoverClose}
-      >
-        <div style={{ padding: 16 }}>
-          <TextField label="Last Name" variant="outlined" fullWidth />
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handlePopoverClose}
-            style={{ marginTop: 16 }}
-          >
-            Save
-          </Button>
-        </div>
-      </Popover>
-    </div>
+
+      {isSuccess && Boolean(searchValue.length) && products.length ? (
+        <Paper
+          elevation={10}
+          sx={{ p: 1, width: 300, position: "absolute", borderRadius: 3 }}
+        >
+          <Stack spacing={1} sx={{ maxHeight: 300, overflow: "auto" }}>
+            {(products as Product[]).map(({ id, title, images }) => (
+              <Link
+                to={`platzi/products/${id}`}
+                style={{
+                  textDecoration: "none",
+                  cursor: "pointer",
+                  color: "black",
+                }}
+              >
+                <Stack direction={"row"} spacing={1} alignItems={"center"}>
+                  <Box sx={{ width: 50, height: 50 }}>
+                    <Box
+                      component={"img"}
+                      src={images[0]}
+                      sx={{
+                        flexShrink: 1,
+                        flexGrow: 0,
+                        margin: "auto",
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover",
+                      }}
+                    />
+                  </Box>
+                  <Typography>{title}</Typography>
+                </Stack>
+              </Link>
+            ))}
+          </Stack>
+        </Paper>
+      ) : null}
+    </Box>
   );
-}
+};
+
+export default Search;
