9035fa6f45837281a1faa372d7aed182c6cf7e59
PavelM <myagkiypavel@gmail.com>
2023-08-07 10:38:36 +0300

    chore: Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,11 @@
+import React from "react";
 import {
   Avatar,
   Box,
   Button,
   Stack,
-  TextField,
   Typography,
   Link as MuLink,
-  Autocomplete,
-  CircularProgress,
-  Paper,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import { ROUTES } from "../../utils/routes";
@@ -17,18 +14,11 @@ import { Favorite, ShoppingBag } from "@mui/icons-material";
 import { useAppSelector } from "../../features/hook";
 import { useDispatch } from "react-redux";
 import { ToggleAction, toggleForm } from "../../features/user/userSlice";
-import { useState } from "react";
-import { useGetProductsQuery } from "../../features/api/apiSlice";
-import React from "react";
 import Search from "./Search";
 
 const Header = () => {
   const { currentUser } = useAppSelector((state) => state.user);
   const dispatch = useDispatch();
-
-  // const handleValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   setSearchValue(e.target.value);
-  // };
 
   const handleCLick = (type: ToggleAction["payload"]["formType"]) => {
     if (!currentUser) {
