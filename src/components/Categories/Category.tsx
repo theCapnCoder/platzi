1cd3ac2322cca9b9b4168059298204b8092cf6dd
PavelM <myagkiypavel@gmail.com>
2023-08-08 21:08:17 +0300

    feature: get name category

--- src/components/Categories/Category.tsx
+++ src/components/Categories/Category.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { Box, Typography } from "@mui/material";
+import React from "react";
+import { useAppSelector } from "../../features/hook";
+import { useParams } from "react-router-dom";
 
 const Category = () => {
-  return (
-    <div>Category</div>
-  )
-}
+  const { categories } = useAppSelector((state) => state);
+  const { isLoading } = useAppSelector((state) => state.categories);
+  const { id } = useParams();
 
-export default Category
+  const nameCategory = isLoading
+    ? categories.list.find((item) => item.id === Number(id))?.name
+    : "";
+
+
+  return (
+    <Box>
+      {isLoading ? (
+        // <Box>{categories.list.find((id) => id.id === +id)}</Box>
+        <Typography>
+          <div>
+            {/* <pre>{JSON.stringify(categories.list, null, 2)}</pre> */}
+            {/* <Typography>
+              <ul>
+                {categories.list.find((item) => {
+                  return <li>{item.name}</li>;
+                })}
+              </ul>
+            </Typography> */}
+
+            <Typography>{nameCategory}</Typography>
+          </div>
+        </Typography>
+      ) : null}
+    </Box>
+  );
+};
+
+export default Category;
