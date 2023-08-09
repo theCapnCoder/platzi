2183df5bc3584099b2e7f9f8268fb1acbe0ca754
PavelM <myagkiypavel@gmail.com>
2023-08-09 11:45:07 +0300

    fix: select all store

--- src/components/Categories/Category.tsx
+++ src/components/Categories/Category.tsx
@@ -1,38 +1,18 @@
 import { Box, Typography } from "@mui/material";
-import React from "react";
 import { useAppSelector } from "../../features/hook";
 import { useParams } from "react-router-dom";
 
 const Category = () => {
-  const { categories } = useAppSelector((state) => state);
-  const { isLoading } = useAppSelector((state) => state.categories);
+  const categories = useAppSelector((state) => state.categories);
+  const isLoading = categories.isLoading;
   const { id } = useParams();
 
   const nameCategory = isLoading
     ? categories.list.find((item) => item.id === Number(id))?.name
     : "";
 
-
   return (
-    <Box>
-      {isLoading ? (
-        // <Box>{categories.list.find((id) => id.id === +id)}</Box>
-        <Typography>
-          <div>
-            {/* <pre>{JSON.stringify(categories.list, null, 2)}</pre> */}
-            {/* <Typography>
-              <ul>
-                {categories.list.find((item) => {
-                  return <li>{item.name}</li>;
-                })}
-              </ul>
-            </Typography> */}
-
-            <Typography>{nameCategory}</Typography>
-          </div>
-        </Typography>
-      ) : null}
-    </Box>
+    <Box>{isLoading ? <Typography>{nameCategory}</Typography> : null}</Box>
   );
 };
 
