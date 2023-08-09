daa6e9525748a19ad07ef7f17324707704743dd7
PavelM <myagkiypavel@gmail.com>
2023-08-09 16:43:24 +0300

    refactor: get nameCategory function

--- src/components/Categories/Category.tsx
+++ src/components/Categories/Category.tsx
@@ -1,18 +1,40 @@
 import { Box, Typography } from "@mui/material";
-import { useAppSelector } from "../../features/hook";
+import { useAppDispatch, useAppSelector } from "../../features/hook";
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { getProductsByCategory } from "../../features/products/actionCreators/getProductsByCategory";
+import Products from "../Products/Products";
 
 const Category = () => {
   const categories = useAppSelector((state) => state.categories);
-  const isLoading = categories.isLoading;
-  const { id } = useParams();
+  const isLoadingCategories = categories.isLoading;
+  const { isLoading: isLoadingProducts, list } = useAppSelector(
+    (state) => state.products
+  );
 
-  const nameCategory = isLoading
-    ? categories.list.find((item) => item.id === Number(id))?.name
+  const { id } = useParams();
+  const dispatch = useAppDispatch();
+
+  const nameCategory = isLoadingCategories
+    ? categories.list.find((item) => item.id === Number(id))?.name || ""
     : "";
 
+  useEffect(() => {
+    if (id) {
+      dispatch(getProductsByCategory({ id: Number(id) }));
+    }
+  }, [id, dispatch]);
+
   return (
-    <Box>{isLoading ? <Typography>{nameCategory}</Typography> : null}</Box>
+    <Box>
+      {isLoadingProducts ? (
+        <Typography>Loading...</Typography>
+      ) : (
+        <Box>
+          <Products products={list} amount={10} title={nameCategory} />
+        </Box>
+      )}
+    </Box>
   );
 };
 
