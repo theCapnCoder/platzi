2f7acb4cf2f15ba88b2ed2d4caa8d705641417d6
PavelM <myagkiypavel@gmail.com>
2023-08-10 18:40:22 +0300

    chore: ProductCard

--- src/components/Products/ProductCard.tsx
+++ src/components/Products/ProductCard.tsx
@@ -1,8 +1,9 @@
 import { Box, Button, Stack, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addItemToCart } from "../../store/user/userSlice";
 import { useNavigate } from "react-router-dom";
+import EditProductModal from "./EditProductModal";
 
 type Props = {
   item: {
@@ -16,9 +17,18 @@ type Props = {
 const ProductCard: React.FC<Props> = ({ item }) => {
   const { title, price, images, description } = item;
 
-  const [currentImage, setCurrentImage] = React.useState(0);
+  const [currentImage, setCurrentImage] = useState(0);
+  const [open, setOpen] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const addToCart = () => {
     dispatch(addItemToCart(item));
@@ -64,17 +74,22 @@ const ProductCard: React.FC<Props> = ({ item }) => {
         <Typography>Category: {description}</Typography>
 
         <Stack direction={"row"} spacing={2}>
-          <Button onClick={addToCart} variant="contained">
+          <Button onClick={addToCart} variant="contained" color="info">
             Add to cart
           </Button>
-          <Button variant="contained" color="warning">
+          <Button variant="contained" color="info">
             Add to favourites
           </Button>
         </Stack>
+        <Button onClick={handleOpen} variant="contained" color="error">
+          Update
+        </Button>
         <Button onClick={() => navigate(-1)} variant="contained">
           Back
         </Button>
       </Stack>
+
+      <EditProductModal open={open} onClose={handleClose} />
     </Stack>
   );
 };
