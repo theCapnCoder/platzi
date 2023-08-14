d3d66a748f53871245cd3346d510d663637150b6
PavelM <myagkiypavel@gmail.com>
2023-08-14 19:07:16 +0300

    fix: generateRandomProduct

--- src/utils/faker.ts
+++ src/utils/faker.ts
@@ -1,22 +1,12 @@
 import { faker } from "@faker-js/faker";
-import { CreateProductFormValues } from "../components/Products/ProductForms/CreateProductForm";
+import { CreateProduct } from "../store/newProducts/actionCreators/createProduct";
 
-// export const generateRandomInitialValues: CreateProductFormValues = () => {
-//   return {
-//     title: faker.commerce.productName(),
-//     price: faker.commerce.price(),
-//     description: faker.lorem.paragraph(),
-//     categoryId: faker.number.int({ max: 3 }),
-//     images: [faker.image.url()],
-//   };
-// };
-
-export const generateRandomInitialValues = (): CreateProductFormValues => {
+export const generateRandomInitialValues = (): CreateProduct => {
   return {
     title: faker.commerce.productName(),
-    price: faker.commerce.price(),
+    price: Number(faker.commerce.price()),
     description: faker.lorem.paragraph(),
-    categoryId: faker.number.int({ min: 1, max: 3 }).toString(),
+    categoryId: faker.number.int({ min: 1, max: 3 }),
     images: faker.image.url(),
   };
 };
