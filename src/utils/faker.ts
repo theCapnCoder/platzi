a1299012fd0598718924d9dc1768fa19e7b27b64
PavelM <myagkiypavel@gmail.com>
2023-08-15 19:23:35 +0300

    chore: add 3 picture in genereateProduct

--- src/utils/faker.ts
+++ src/utils/faker.ts
@@ -2,11 +2,12 @@ import { faker } from "@faker-js/faker";
 import { CreateProduct } from "../store/newProducts/actionCreators/createProduct";
 
 export const generateRandomInitialValues = (): CreateProduct => {
+  const getImage = () => faker.image.url()
   return {
     title: faker.commerce.productName(),
     price: Number(faker.commerce.price()),
     description: faker.lorem.paragraph(),
     categoryId: faker.number.int({ min: 1, max: 3 }),
-    images: faker.image.url(),
+    images: [getImage(), getImage(), getImage()],
   };
 };
