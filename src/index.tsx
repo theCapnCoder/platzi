13e1baa106b66f029311f5844a2bf1074581e09e
PavelM <myagkiypavel@gmail.com>
2023-08-21 12:51:47 +0300

    chore: index

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "./components/App/App";
+import App from "./pages/App/App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
